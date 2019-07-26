f8bcbc57eb4459391a23726a9d03fb7a73c9d665
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-25 22:52:09 -0400

    Update ldap_install.sh

--- ldap_install.sh
+++ ldap_install.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-sudo su - && apt update
+sudo apt update
 cd
+
+cat > ~/debconf-slapd.conf << ‘EOF’
+slapd slapd/password1 password admin
+slapd slapd/internal/adminpw password admin
+slapd slapd/internal/generated_adminpw password admin
+slapd slapd/password2 password admin
+slapd slapd/unsafe_selfwrite_acl note
+slapd slapd/purge_database boolean false
+slapd slapd/domain string clemson.cloudlab.us
+slapd slapd/ppolicy_schema_needs_update select abort installation
+slapd slapd/invalid_config boolean true
+slapd slapd/move_old_database boolean false
+slapd slapd/backend select MDB
+slapd shared/organization string ETH Zurich
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
+slapd slapd/no_configuration boolean false
+slapd slapd/dump_database select when needed
+slapd slapd/password_mismatch note
+EOF
+
 export DEBIAN_FRONTEND=noninteractive
+cat ~/debconf-slapd.conf | debconf-set-selections
+sudp apt install ldap-utils slapd -y
 
-echo -e "slapd slapd/root_password password admin" | debconf-set-selections
-echo -e "slapd slapd/root_password_again password admin" | debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password admin" | debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password admin" | debconf-set-selections
-echo -e "slapd slapd/password2 password amin" | debconf-set-selections
-echo -e "slapd slapd/password1 password admin" | debconf-set-selections
-echo -e "slapd slapd/domain string acu.local" | debconf-set-selections
+cat > ~/basedn.ldif << ‘EOF’
+dn: ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: organizationalUnit
+ou: People
 
-apt-get install -y slapd ldap-utils
+dn: ou=Groups,dc=clemson,dc=cloudlab,dc=us
+objectClass: organizationalUnit
+ou: Groups
+
+dn: cn=CSC,ou=Groups,dc=clemson,dc=cloudlab,dc=us
+objectClass: posixGroup
+cn: CSC586
+gidNumber: 5000
+EOF
+
 
