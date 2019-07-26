3a085d23fecc2c26d74519ae4cd8883c86e4f34c
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-26 13:24:24 -0400

    Update ldap_install.sh

--- ldap_install.sh
+++ ldap_install.sh
@@ -53,7 +53,7 @@ cn: student
 displayName: student
 uidNumber: 10000
 gidNumber: 5000
-userPassword: "$PASSHASH"
+userPassword: $PASSHASH
 gecos: Golden Ram
 loginShell: /bin/dash
 homeDirectory: /home/student
