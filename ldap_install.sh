c2c233f10c952ee353313dd8371dc1c120058573
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-26 21:09:51 -0400

    Update ldap_install.sh

--- ldap_install.sh
+++ ldap_install.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
 sudo apt update
-mkdir /users/mg920115/.ldapAutomate
-cat <<EOF >> /users/mg920115/.ldapAutomate/debconf-slapd.conf 
-slapd slapd/password1 password admin
-slapd slapd/internal/adminpw password admin
-slapd slapd/internal/generated_adminpw password admin
-slapd slapd/password2 password admin
-slapd slapd/unsafe_selfwrite_acl note
-slapd slapd/purge_database boolean false
-slapd slapd/domain string clemson.cloudlab.us
-slapd slapd/ppolicy_schema_needs_update select abort installation
-slapd slapd/invalid_config boolean true
-slapd slapd/move_old_database boolean false
-slapd slapd/backend select MDB
-slapd shared/organization string WestChesterUniversity
-slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
-slapd slapd/no_configuration boolean false
-slapd slapd/dump_database select when needed
-slapd slapd/password_mismatch note
-EOF
+# mkdir /users/mg920115/.ldapAutomate
+# cat <<EOF >> /users/mg920115/.ldapAutomate/debconf-slapd.conf 
+# slapd slapd/password1 password admin
+# slapd slapd/internal/adminpw password admin
+# slapd slapd/internal/generated_adminpw password admin
+# slapd slapd/password2 password admin
+# slapd slapd/unsafe_selfwrite_acl note
+# slapd slapd/purge_database boolean false
+# slapd slapd/domain string clemson.cloudlab.us
+# slapd slapd/ppolicy_schema_needs_update select abort installation
+# slapd slapd/invalid_config boolean true
+# slapd slapd/move_old_database boolean false
+# slapd slapd/backend select MDB
+# slapd shared/organization string WestChesterUniversity
+# slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
+# slapd slapd/no_configuration boolean false
+# slapd slapd/dump_database select when needed
+# slapd slapd/password_mismatch note
+# EOF
 
 export DEBIAN_FRONTEND=noninteractive
-cat /users/mg920115/.ldapAutomate/debconf-slapd.conf | sudo debconf-set-selections
+cat /local/repository/slapd.debsetting | sudo debconf-set-selections
 sudo apt install ldap-utils slapd -y
 
-cat <<EOF >> /users/mg920115/.ldapAutomate/basedn.ldif
-dn: ou=People,dc=clemson,dc=cloudlab,dc=us
-objectClass: organizationalUnit
-ou: People
+# cat <<EOF >> /users/mg920115/.ldapAutomate/basedn.ldif
+# dn: ou=People,dc=clemson,dc=cloudlab,dc=us
+# objectClass: organizationalUnit
+# ou: People
 
-dn: ou=Groups,dc=clemson,dc=cloudlab,dc=us
-objectClass: organizationalUnit
-ou: Groups
+# dn: ou=Groups,dc=clemson,dc=cloudlab,dc=us
+# objectClass: organizationalUnit
+# ou: Groups
 
-dn: cn=CSC,ou=Groups,dc=clemson,dc=cloudlab,dc=us
-objectClass: posixGroup
-cn: CSC586
-gidNumber: 5000
-EOF
+# dn: cn=CSC,ou=Groups,dc=clemson,dc=cloudlab,dc=us
+# objectClass: posixGroup
+# cn: CSC586
+# gidNumber: 5000
+# EOF
 
 
 PASSHASH=$(slappasswd -s rammy)
-cat <<EOF >> /users/mg920115/.ldapAutomate/users.ldif
+cat <<EOF > /local/repository/users.ldif
 dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
 objectClass: inetOrgPerson
 objectClass: posixAccount
@@ -61,5 +61,5 @@ homeDirectory: /home/student
 EOF
 
 sudo ufw allow ldap
-ldapadd -f /users/mg920115/.ldapAutomate/basedn.ldif -x -D "cn=admin,dc=clemson,dc=cloudlab,dc=us" -w admin
-ldapadd -f /users/mg920115/.ldapAutomate/users.ldif -x -D "cn=admin,dc=clemson,dc=cloudlab,dc=us" -w admin
+ldapadd -f /local/repository/basedn.ldif -x -D "cn=admin,dc=clemson,dc=cloudlab,dc=us" -w admin
+ldapadd -f /local/repository/users.ldif -x -D "cn=admin,dc=clemson,dc=cloudlab,dc=us" -w admin
