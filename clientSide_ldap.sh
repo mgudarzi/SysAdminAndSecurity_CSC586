22d95e1193d117b8c94d81101cce0d6c6dfd5662
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-27 17:45:10 -0400

    Update clientSide_ldap.sh

--- clientSide_ldap.sh
+++ clientSide_ldap.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
+#updates the system repo database
 sudo apt update
 
-# cat <<EOF >> /users/mg920115/.ldapAutomate/ldap-auth-config.debsetting
-# ldap_auth_config  ldap_auth_config/bindpw password admin
-# ldap_auth_config  ldap_auth_config/rootbindpw password
-# ldap_auth_config  ldap_auth_config/ldapns/ldap-server string ldap://192.168.1.1
-# ldap_auth_config  ldap_auth_config/pam_password select md5
-# ldap_auth_config  ldap_auth_config/move-to-debconf boolean true
-# ldap_auth_config  ldap_auth_config/rootbinddn string cn=admin,dc=clemson,dc=cloudlab,dc=us
-# ldap_auth_config  ldap_auth_config/override boolean true 
-# ldap_auth_config  ldap_auth_config/dbrootlogin boolean true
-# ldap_auth_config  ldap_auth_config/dblogin boolean false
-# libpam-runtime  libpam-runtime/profiles multiselect unix, ldap, systemd,capability
-# ldap_auth_config  ldap_auth_config/ldapns/ldap_version select 3
-# ldap_auth_config  ldap_auth_config/binddn string cn=proxyuser,dc=example,dc=net
-# ldap_auth_config  ldap_auth_config/ldapns/base-dn string dc=clemson,dc=cloudlab,dc=us
-# EOF
-
+#change the fronend to noninteractive, avoiding prompts for automation
 export DEBIAN_FRONTEND=noninteractive
-# sudo apt-get install -y debconf-utils
-# sudo apt-get install -y aptitude
 
-cat /local/repository/ldap-auth-config.debsetting | sudo debconf-set-selections
-sudo DEBIAN_FRONTEND=noninteractive aptitude install -y -q ldap-auth-client
-sudo apt install -y libnss-ldap libpam-ldap ldap-utils
-# sudo apt install ldap-auth-config ldap-utils -y
+#pre-seeding debconf with ldap_auth_conifg.debconf file 
+cat /local/repository/ldap-auth-config.debconf | sudo debconf-set-selections
+
+#installs libnss-ldap libpam-ldap ldap-utils along with all their dependencies
+sudo apt install -y -q libnss-ldap libpam-ldap ldap-utils
+
+#changes to ldap.conf, nsswitch.conf , common-session, common-password 
 sudo sed -i 's/uri ldapi:\/\/\//uri ldap:\/\/192.168.1.1\//g' /etc/ldap.conf
 sudo sed -i 's/base dc=example,dc=net/base dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
 sudo sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=admin,dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
@@ -34,6 +21,8 @@ sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/# end of pam-auth-update config/ i session optional pam-mkhomedir.so  skel=/etc/skel  umsak=077' /etc/pam.d/common-session
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
 
+#fetches and prints details for a particular user
 getent passwd student
 
+#switches to "student" account
 sudo su - student
