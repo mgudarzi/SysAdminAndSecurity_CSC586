9dd23c5f449d323a8be456c072502385ffb225f7
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-26 23:02:31 -0400

    Update ldap_install_combined.sh

--- ldap_install_combined.sh
+++ ldap_install_combined.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [[ $(uname -n | head -c 10) == "ldapserver" ]];then
+  chmod 755 /local/repository/ldap_install.sh
   /local/repository/ldap_install.sh
   exit 0
 else
-  sleep 2m
+  sleep 1m
+  chmod 755 /local/repositoryclientSide_ldap.sh
   /local/repositoryclientSide_ldap.sh
   exit 0
 fi
