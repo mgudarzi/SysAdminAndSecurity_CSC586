7f935bda4ff6199a215349efec903c3ce55f71d8
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-27 21:38:42 -0400

    Update clientSide_ldap.sh

--- clientSide_ldap.sh
+++ clientSide_ldap.sh
@@ -21,10 +21,9 @@ sudo sed -i '/passwd:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/# end of pam-auth-update config/ i session optional pam_mkhomedir.so  skel=/etc/skel  umsak=077' /etc/pam.d/common-session
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
-# sudo bash <<EOF
-# echo $ADMIN_PASSWORD > /etc/ldap.secret
-# EOF
-#echo $ADMIN_PASSWORD > /etc/ldap.secret
+sudo bash <<EOF
+echo $ADMIN_PASSWORD > /etc/ldap.secret
+EOF
 sudo chmod 600 /etc/ldap.secret
 
 #fetches and prints details for a particular user
