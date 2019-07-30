9c557d283dec0dea64f93d17f5dd4b815fd2f879
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2019-07-30 15:59:34 -0400

    Update ldap_install_combined.sh

--- ldap_install_combined.sh
+++ ldap_install_combined.sh
@@ -8,17 +8,21 @@ else
   #sleep 2m
   #chmod 755 /local/repository/clientSide_ldap.sh
   /local/repository/clientSide_ldap.sh
-  if [[ $(uname -n | head -c 10) == "nfsserver" ]];then
+  if [[ $(uname -n | head -c 9) == "nfsserver" ]];then
     sudo apt-get install -y nfs-kernel-server
     sudo chown nobody:nogroup /nfs/home
-    sudo bash <<EOF
+    
+    sudo bash<<EOF
     echo "/nfs/home 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
     EOF
+    
     sudo systemctl restart nfs-kernel-server
     
   else
+  
     sudo apt-get install -y nfs-common
     sudo mkdir -p /nfs/home
     sudo mount 192.168.1.2:/nfs/home /nfs/home
+
   exit 0
 fi
